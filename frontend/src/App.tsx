9c78dd8b156359c6932a49609b34499e056c6328
CharasmaticWhenTipsy <76942015+CharasmaticWhenTipsy@users.noreply.github.com>
2025-02-05 13:32:27 +1100

    As far back as I can remember, I always wanted to be a gangster.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Input } from "@mui/material";
+
+function App() {
+  // we have working react with vite now.
+  // return main login screen
+  // enter pin to get started or create lobby
+  // on success code return new lobby
+  // else game does not exist
+
+  return (
+    <>
+      <p>Enter Code to Begin</p>
+      <Input></Input>
+    </>
+  );
+}
+
+export default App;
